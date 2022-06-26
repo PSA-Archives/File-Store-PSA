8844e9418bc35fe939b1b298bbc8af6fe78d9558
PSA-Archives <108168815+PSA-Archives@users.noreply.github.com>
2022-06-26 11:05:18 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>//𝗔𝗡𝗔𝗡𝗗𝗛𝗨//</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
